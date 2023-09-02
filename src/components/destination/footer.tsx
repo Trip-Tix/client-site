7b40dac57fc9f33e6165d20ba2196d1fe59dbfcd
Shamit187 <shamit187@gmail.com>
2023-09-02 14:15:34 +0600

    Making Eye Popping THings in my goal

--- src/components/destination/footer.tsx
+++ src/components/destination/footer.tsx
@@ -1,15 +1,5 @@
 import { useSpring, animated } from "@react-spring/web";
 
 export default function Footer() {
-  const props = useSpring({
-    from: { opacity: 0, marginTop: 500 },
-    to: { opacity: 1, marginTop: 0 },
-    delay: 2000,
-  });
-
-  return (
-    <animated.div style={props}>
-      <div>Footer</div>
-    </animated.div>
-  );
+  return <div>Footer</div>;
 }
--- src/components/destination/location.tsx
+++ src/components/destination/location.tsx
@@ -1,28 +1,30 @@
 import { useSpring, animated } from "@react-spring/web";
-import { useState } from "react";
+import useMeasure from "react-use-measure";
+import { useEffect, useState } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
+import Style from "@styles/destination/location.module.css";
 
 interface LocationData {
-    label: string;
-    id: number;
+  label: string;
+  id: number;
 }
 
 const locations: LocationData[] = [
-    { label: "Dhaka", id: 1 },
-    { label: "Chittagong", id: 2 },
-    { label: "Sylhet", id: 3 },
-    { label: "Rajshahi", id: 4 },
-    { label: "Khulna", id: 5 },
-    { label: "Barishal", id: 6 },
-    { label: "Rangpur", id: 7 },
-    { label: "Mymensingh", id: 8 },
+  { label: "Dhaka", id: 1 },
+  { label: "Chittagong", id: 2 },
+  { label: "Sylhet", id: 3 },
+  { label: "Rajshahi", id: 4 },
+  { label: "Khulna", id: 5 },
+  { label: "Barishal", id: 6 },
+  { label: "Rangpur", id: 7 },
+  { label: "Mymensingh", id: 8 },
 ];
 
 export default function Location() {
-  const prop = useSpring({
+  const sourceAnimation = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     delay: 2000,
@@ -30,6 +32,18 @@ export default function Location() {
 
   const [source, setSource] = useState("");
   const [destination, setDestination] = useState("");
+
+  const [ref, { width }] = useMeasure();
+  const boxSlide = useSpring({ width: source === "" ? 0 : width,
+  delay: 500, });
+  useEffect(() => {
+    console.log(source === "");
+  }, [source]);
+
+  const destinationAnimation = useSpring({
+    opacity: source === "" ? 0 : 1,
+    delay: 600,
+  });
 
   return (
     <Stack
@@ -39,7 +53,7 @@ export default function Location() {
       direction="row"
       justifyContent={"center"}
     >
-      <animated.div style={prop}>
+      <animated.div style={sourceAnimation}>
         <Autocomplete
           id="combo-box-demo"
           options={locations}
@@ -55,19 +69,27 @@ export default function Location() {
       <Box
         sx={{
           width: 1,
-          background: "#FFFFFF",
         }}
-      />
-      <Autocomplete
-        id="combo-box-demo"
-        options={locations}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Destination" />}
-        inputValue={destination}
-        onInputChange={(event, newInputValue) => {
-          setDestination(newInputValue);
-        }}
-      />
+        ref={ref}
+        className={Style.box}
+      >
+        <animated.div style={boxSlide} className={Style.slide} />
+      </Box>
+
+      <animated.div style={destinationAnimation}>
+        <Autocomplete
+          id="combo-box-demo"
+          options={locations}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Destination" />
+          )}
+          inputValue={destination}
+          onInputChange={(event, newInputValue) => {
+            setDestination(newInputValue);
+          }}
+        />
+      </animated.div>
     </Stack>
   );
 }
